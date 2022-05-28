6c0ad2f48520ca20d3c4f3244935704f8fff7053
igorgorovoy <iggorovoy@gmail.com>
2022-05-28 22:12:21 +0200

    init project

---
+++ src/main/java/com/example/testdrivenspringboot/TestDrivenSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdrivenspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDrivenSpringBootApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestDrivenSpringBootApplication.class, args);
+  }
+
+}
